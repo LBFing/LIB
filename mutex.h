756411c96cc1ab0fe19118770075779b6408ebd3
LBFing <fing519@126.com>
2016-04-25 09:07:12 +0800

    mutex

--- mutex.h
+++ mutex.h
@@ -4,11 +4,56 @@
 class Mutex : private Noncopyable
 {
 public:
-	Mutex();
-	~Mutex();
-	void Lock();
-	void UnLock();
-	bool TryLock();
+	Mutex()
+	{
+		pthread_mutex_init(&m_mutex, NULL);
+	}
+	~Mutex()
+	{
+		pthread_mutex_destroy(&m_mutex);
+	}
+	void Lock()
+	{
+		pthread_mutex_lock(&m_mutex);
+	}
+	void UnLock()
+	{
+		pthread_mutex_unlock(&m_mutex);
+	}
+	bool TryLock()
+	{
+		return (pthread_mutex_trylock(&m_mutex) == 0);
+	}
 private:
 	pthread_mutex_t m_mutex; 
+};
+
+
+class RWLock : private Noncopyable
+{
+public:
+	RWLock()
+	{
+		pthread_rwlock_init(&m_rwlock,NULL);
+	}
+	~RWLock()
+	{
+		pthread_rwlock_destroy(&m_rwlock);
+	}
+	void rdlock()
+	{
+		pthread_rwlock_rdlock(&m_rwlock);
+	}
+	void wrlock()
+	{
+		pthread_rwlock_wrlock(&m_rwlock);
+	}
+
+	void unlock()
+	{
+		pthread_rwlock_unlock(&m_rwlock);
+	}
+	
+private:
+	pthread_rwlock_t m_rwlock;
 };
