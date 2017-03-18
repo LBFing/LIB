674a47b4381d6ae4f0a4cbdbacd7899925143e9b
LBFing <fing519@126.com>
2017-03-18 14:11:50 +0800

    log

--- log_stream.cpp
+++ log_stream.cpp
@@ -1,2 +1,21 @@
 #include "log_stream.h"
 
+template class FixedBuffer < kSmallBuffer > ;
+template class FixedBuffer < KLargeBuffer >;
+
+template<int SIZE>
+const char* FixedBuffer<SIZE>::debugString()
+{
+	*m_cur = '\0';
+	return m_cur;
+}
+
+template<int SIZE>
+void FixedBuffer<SIZE>::cookieStart()
+{
+}
+
+template<int SIZE>
+void FixedBuffer<SIZE>::cookieEnd()
+{
+}
--- log_stream.h
+++ log_stream.h
@@ -14,16 +14,19 @@ template <int SIZE>
 class FixedBuffer : public Noncopyable
 {
 public:
+	//设置开始回调
 	FixedBuffer(): m_cur(m_data)
 	{
 		SetCookie(cookieStart);
 	}
 
+	//设置结束回调
 	~FixedBuffer()
 	{
 		SetCookie(cookieEnd);
 	}
 
+	//添加字符空间
 	void Append(const char* buf, size_t len)
 	{
 		int32 avali_able = avali();
@@ -34,18 +37,38 @@ public:
 		}
 	}
 
-	int32 avali() const
-	{
-		return static_cast<int32>(m_cur - m_data);
-	}
+	//计算buff剩余空间
+	int Avail() const { return static_cast<int>(end() - Current()); }
 
+	//设置工作函数
 	void SetCookie(FunCookie cookie)
 	{
 		m_fun_cookie = cookie;
 	}
 
+	//返回数据地址
 	const char* Data() const { return m_data; }
+
+	//返回当前已写的长度
 	int Length() const { return static_cast<int> (m_cur - m_data); }
+
+	//返回当前的写的地址
+	char* Current() { return m_cur };
+
+	//已写的空间便宜
+	void Add(size_t len) { m_cur += len; };
+
+	//缓存区重置
+	void Reset() { m_cur = m_data };
+
+	//缓冲区数据清0
+	void BZero() { ::bzero(m_data, sizeof(m_data)); }
+
+	//调试使用
+	char char* DebugString();
+
+	//账号当前已写数据为string
+	string ToString() const { return string(m_data, Length()); }
 
 
 private:
