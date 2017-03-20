25e0e52538c24b5549ffd588f575e285ce0c84b3
LBFing <fing519@126.com>
2017-03-20 20:22:21 +0800

    test

--- test/file_util_test.cpp
+++ test/file_util_test.cpp
@@ -2,28 +2,35 @@
 
 int main()
 {
-  string result;
-  int64 size = 0;
-  string file("/proc/self");
-  int32 err = ReadFile(file, 1024, &result, &size);
-  printf("%d %lu %lld\n", err, result.size(), size);
-  file = "/proc/self";
-  err = ReadFile(file, 1024, &result, NULL);
-  printf("%d %lu %lld\n", err, result.size(), size);
+	string result;
+	int64 size = 0;
+	string file("/proc/self");
+	int32 err = ReadFile(file, 1024, &result, &size);
+	printf("%d %lu %lld\n", err, result.size(), size);
+	file = "/proc/self";
+	err = ReadFile(file, 1024, &result, NULL);
+	printf("%d %lu %lld\n", err, result.size(), size);
 
-  file = "/proc/self/cmdline";
-  err = ReadFile(file, 1024, &result, &size);
-  printf("%d %lu %lld\n", err, result.size(), size);
+	file = "/proc/self/cmdline";
+	err = ReadFile(file, 1024, &result, &size);
+	printf("%d %lu %lld\n", err, result.size(), size);
 
-  //err = ReadFile("/dev/null", 1024, &result, &size);
-  //printf("%d %zd %lld\n", err, result.size(), size);
-  //err = ReadFile("/dev/zero", 1024, &result, &size);
-  //printf("%d %zd %lld\n", err, result.size(), size);
-  //err = ReadFile("/notexist", 1024, &result, &size);
-  //printf("%d %zd %lld\n", err, result.size(), size);
-  //err = ReadFile("/dev/zero", 102400, &result, &size);
-  //printf("%d %zd %lld\n", err, result.size(), size);
-  //err = ReadFile("/dev/zero", 102400, &result, NULL);
-  //printf("%d %zd %lld\n", err, result.size(), size);
-  return 0;
+	file = "/dev/null";
+	err = ReadFile(file, 1024, &result, &size);
+	printf("%d %zd %lld\n", err, result.size(), size);
+	file = "/dev/zero";
+	err = ReadFile(file, 1024, &result, &size);
+	printf("%d %zd %lld\n", err, result.size(), size);
+	file = "/notexist";
+	err = ReadFile(file, 1024, &result, &size);
+	printf("%d %zd % lld\n", err, result.size(), size);
+
+	file = "/dev/zero";
+	err = ReadFile(file, 102400, &result, &size);
+	printf("%d %zd %lld\n", err, result.size(), size);
+
+	file = "/dev/zero";
+	err = ReadFile(file, 102400, &result, NULL);
+	printf("%d %zd %lld\n", err, result.size(), size);
+	return 0;
 }
