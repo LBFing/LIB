33be832378008cde29dd9aa592d6d7d040911bab
LBFing <fing519@126.com>
2017-04-09 23:50:55 +0800

    threadpool

---
+++ thread_pool.cpp
@@ -0,0 +1,149 @@
+#include "thread_pool.h"
+
+ThreadPool::ThreadPool(const string& nameArg)
+	: m_mutex()
+	, m_notEmpty(m_mutex)
+	, m_notFull(m_mutex)
+	, m_strName(nameArg)
+	, m_maxQueueSize(0)
+	, m_running(false)
+{
+}
+
+ThreadPool::~ThreadPool()
+{
+	if(m_running)
+	{
+		Stop();
+	}
+}
+
+void ThreadPool::Start(int32 numThreads)
+{
+	assert(m_threads.empty());
+	m_running = true;
+	m_threads.reserve(numThreads);
+	for(int32 i = 0; i < numThreads; ++i)
+	{
+		char id[32] = {0};
+		snprintf(id, sizeof(id), "%d", i + 1);
+		Thread* pThread = new Thread(std::bind(&ThreadPool::runInThread, this), m_strName + id);
+		if (pThread)
+		{
+			m_threads.push_back(pThread);
+			m_threads[i]->Start();
+		}
+		else
+		{
+			fprintf(stderr, "ThreadPool Start failed\n");
+			abort();
+		}
+	}
+	if (numThreads == 0 && m_threadInitCallBack)
+	{
+		m_threadInitCallBack();
+	}
+}
+
+void ThreadPool::Stop()
+{
+	{
+		MutexLockGuard lock(m_mutex);
+		m_running = false;
+		m_notEmpty.NotifyAll();
+	}
+
+	for(std::vector<Thread*>::iterator it = m_threads.begin(); it != m_threads.end(); ++it)
+	{
+		if(*it)
+		{
+			(*it)->Join();
+			delete (*it);
+			(*it) = NULL;
+		}
+	}
+	m_threads.clear();
+}
+
+size_t ThreadPool::QueueSize()const
+{
+	MutexLockGuard lock(m_mutex);
+	return m_queue.size();
+}
+
+void ThreadPool::Run(const Task& task)
+{
+	if(m_threads.empty())
+	{
+		take();
+	}
+	else
+	{
+		MutexLockGuard lock(m_mutex);
+		while (isFull())
+		{
+			m_notFull.Wait();
+		}
+		assert(!isFull());
+
+		m_queue.push_back(std::move(task));
+		m_notFull.Notify();
+	}
+}
+
+bool ThreadPool::isFull()const
+{
+	m_mutex.AssertLocked();
+	return m_maxQueueSize > 0 && m_queue.size() >= m_maxQueueSize;
+}
+
+ThreadPool::Task ThreadPool::take()
+{
+	MutexLockGuard lock(m_mutex);
+	while(m_queue.empty() && m_running)
+	{
+		m_notEmpty.Wait();
+	}
+
+	Task task;
+	if(!m_queue.empty())
+	{
+		task = m_queue.front();
+		m_queue.pop_front();
+		if(m_maxQueueSize > 0)
+		{
+			m_notFull.Notify();
+		}
+	}
+	return task;
+}
+
+void ThreadPool::runInThread()
+{
+	try
+	{
+		if(m_threadInitCallBack)
+		{
+			m_threadInitCallBack();
+		}
+		while(m_running)
+		{
+			Task task(take());
+			if(task)
+			{
+				task();
+			}
+		}
+	}
+	catch(const std::exception& ex)
+	{
+		fprintf(stderr, "exception caught in ThreadPool %s\n", m_strName.c_str());
+		fprintf(stderr, "reason: %s\n", ex.what());
+		abort();
+	}
+	catch(...)
+	{
+		fprintf(stderr, "unknown exception caught in ThreadPool %s\n", m_strName.c_str());
+		throw;
+	}
+}
---
+++ thread_pool.h
@@ -0,0 +1,54 @@
+#ifndef __THREADPOLL_H__
+#define __THREADPOLL_H__
+#include "type_define.h"
+#include "nocopyable.h"
+#include "mutex.h"
+#include "condition.h"
+#include "thread.h"
+
+class ThreadPool : private Nocopyable
+{
+public:
+	typedef std::function<void ()> Task;
+	explicit ThreadPool(const string& nameArg = string("ThreadPool"));
+	~ThreadPool();
+
+	void SetMaxQueueSize(int32 maxSize)
+	{
+		m_maxQueueSize = maxSize;
+	}
+
+	void SetThreadInitCallBack(const Task& cb)
+	{
+		m_threadInitCallBack = cb;
+	}
+
+	void Start(int32 numThreads);
+	void Stop();
+	const string& name()const
+	{
+		return m_strName;
+	}
+
+	size_t QueueSize()const;
+
+	void Run(const Task& f);
+
+private:
+	bool isFull() const;
+	void runInThread();
+	Task take();
+
+	mutable Mutex m_mutex;
+	Condition m_notEmpty;
+	Condition m_notFull;
+	string m_strName;
+	Task m_threadInitCallBack;
+	std::vector<Thread*> m_threads;
+	std::deque<Task> m_queue;
+	size_t m_maxQueueSize;
+	bool m_running;
+};
+
+
+#endif
