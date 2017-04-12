ad156834f6df66974959049a19952314e8a5af9e
LBFing <fing519@126.com>
2017-04-12 20:15:28 +0800

    poll

---
+++ poller.h
@@ -0,0 +1,18 @@
+#ifndef __POLLER_H__
+#define __POLLER_H__
+
+#include "time.h"
+#include "nocopyable.h"
+
+class Channel;
+
+class Poller : private Nocopyable
+{
+public:
+
+private:
+
+};
+
+
+#endif
