2285a515f35d31bef0018c1f338580b776ca9ce4
LBFing <fing519@126.com>
2017-04-19 09:10:00 +0800

    bug

--- timer_queue.cpp
+++ timer_queue.cpp
@@ -9,7 +9,7 @@ int32 createTimerfd()
 	//CLOCK_REALTIME：相对时间，从1970.1.1到目前的时间。更改系统时间会更改获取的值
 	//TFD_NONBLOCK（非阻塞），TFD_CLOEXEC（同O_CLOEXEC）
 	int32 timerfd = ::timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
-	INFO("createTimerfd %d",timerfd);
+	INFO("createTimerfd %d", timerfd);
 	if (timerfd < 0)
 	{
 		ERROR("Failed in timerfd_create");
@@ -151,6 +151,7 @@ void TimerQueue::handleRead()
 {
 	m_pLoop->AssertInLoopThread();
 	Timestamp now(Timestamp::Now());
+	readTimerfd(m_timerFd, now);
 	std::vector<TimerEntry> vecExpired = getExpired(now);
 	m_callingExpiredTimer = true;
 	m_cancelingTimer.clear();
